9fd4c35713e3b136eb06830ac831d5af49576156
zane.barker@gmail.com <zane.barker@gmail.com>
2022-02-13 01:22:36 +0900

    Initial commit. Rough design only. In desperate need of testing.

---
+++ control.vhdl
@@ -0,0 +1,325 @@
+use work.controlConstants.all;
+use work.registerConstants.all;
+use work.fetchConstants.all;
+use work.aluConstants.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity controlUnit is
+    port (
+        rst             : in std_logic;
+        clk             : in std_logic;
+        -- dataBus         : in std_logic_vector (regDataBusHalfWidth-1 downto 0);
+
+        -- Register Signals
+        regOpCode       : out std_logic_vector (regOpCodeWidth-1 downto 0);
+        regAddrBus      : out std_logic_vector (regAddrBusWidth-1 downto 0);
+
+        -- ALU Signals
+        aluFlagBus : in std_logic_vector (aluRegisterWidth-1 downto 0);
+        aluOpCode : out std_logic_vector (aluOpCodeWidth-1 downto 0);
+
+        -- Fetch Signals
+        instructionBus  : in std_logic_vector(fetchInstructionWidth-1 downto 0);
+        fetchOpCode : out std_logic_vector(fetchOpWidth-1 downto 0);
+        addrBusLock : out std_logic
+    );
+end controlUnit;
+
+architecture rtl of controlUnit is
+    type state_type is (fetch, load, execute);
+    signal stateMachine : state_type;
+
+    signal nextState : std_logic;
+begin
+
+    instructionDecodeProcess : process (stateMachine) is
+    begin
+        if state = load then
+
+        end if;
+    end process;
+
+
+    stateDecodeProcess : process (stateMachine) is
+    begin
+        case stateMachine is
+            -- FETCH STATE --
+            when fetch =>
+                -- ALU off
+                aluOpCode <= aluNOP;
+                -- Put program counter onto address bus
+                regOpCode <= regWideOut;
+                regAddrBus <= regPC_slv;
+                -- Read out address onto instruction bus
+                fetchOpCode <= fetchLDI;
+                addrBusLock <= '1'
+                -- Move to the next state
+                nextState <= '1';
+
+            -- LOAD STATE --    
+            when load =>
+                if instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlDualOperandOpCodeWidth) = cpuOpLDxY then
+                    -- First put the target 16-bit register onto the address bus
+                    regOpCode <= regWideOut;
+                    regAddrBus <= instructionBus(1 downto 0);
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit does nothing other than latch the address bus
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '1';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlDualOperandOpCodeWidth) = cpuOpSTxY then
+                    -- First put the target 16-bit register onto the address bus
+                    regOpCode <= regWideOut;
+                    regAddrBus <= instructionBus(1 downto 0);
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit does nothing other than latch the address bus
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '1';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpJMPX then
+                    -- Copy the target register to the PC register
+                    regOpCode <= regCpyToPC;
+                    regAddrBus <= instuctionBus(fetchInstructionWidth-controlDualOperandOpCodeWidth-1 downto 0);
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpCJMPX then
+                    -- If the Zero Flag is set, copy the target register to the PC register
+                    if aluFlagBus(zeroFlagIndex) = '1' then
+                        regOpCode <= regCpyToPC;
+                    else
+                        regOpCode <= regNOP;
+                    end if;
+                    regAddrBus <= instuctionBus(fetchInstructionWidth-controlDualOperandOpCodeWidth-1 downto 0);
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpMVxQ then
+                    -- Load target register to data bus, selecting the correct 8-bit register
+                    if instructionBus(0) = 1 then
+                        regOpCode <= regHalfOutH;
+                    else
+                        regOpCode <= regHalfOutL;
+                    end if;
+                    regAddrBus <= instructionBus(2 downto 1);
+                    -- ALU stores data bus to accumulator register
+                    aluOpCode <= aluLDA;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpMVQx then
+                    -- Save data bus value to target register, selecting the correct 8-bit register
+                    if instructionBus(0) = 1 then
+                        regOpCode <= regHalfInH;
+                    else
+                        regOpCode <= regHalfInL;
+                    end if;
+                    regAddrBus <= instructionBus(2 downto 1);
+                    -- ALU reads out accumulator register onto the data bus
+                    aluOpCode <= aluRDA;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpMVFx then                
+                    -- Save data bus value to target register, selecting the correct 8-bit register
+                    if instructionBus(0) = 1 then
+                        regOpCode <= regHalfInH;
+                    else
+                        regOpCode <= regHalfInL;
+                    end if;
+                    regAddrBus <= instructionBus(2 downto 1);
+                    -- ALU reads out accumulator register onto the data bus
+                    aluOpCode <= aluRDF;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpADDx or
+                      instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpSUBx or
+                      instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpANDx or
+                      instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpXORx or
+                      instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpORx  then
+                    -- Load target register to data bus, selecting the correct 8-bit register
+                    if instructionBus(0) = 1 then
+                        regOpCode <= regHalfOutH;
+                    else
+                        regOpCode <= regHalfOutL;
+                    end if;
+                    regAddrBus <= instructionBus(2 downto 1);
+                    -- ALU stores data bus to accumulator temporary register
+                    aluOpCode <= aluLDT;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlZeroOperandOpCodeWidth) = cpuOpNOTQ or
+                      instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlZeroOperandOpCodeWidth) = cpuOpINCQ or
+                      instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlZeroOperandOpCodeWidth) = cpuOpDECQ then
+                else  -- This covers the NOP, NOTQ, INCQ, and DECQ cases.
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                end if;
+
+            -- EXECUTE STATE --
+            when execute =>
+                if instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlDualOperandOpCodeWidth) = cpuOpLDxY then
+                    -- Store the data bus into target 8-bit register
+                    regOpCode <= regHalfIn;
+                    if instructionBus(2) = 1 then
+                        regOpCode <= regHalfOutH;
+                    else
+                        regOpCode <= regHalfOutL;
+                    end if;
+                    regAddrBus <= instructionBus(4 downto 3);
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit loads byte from memory to the data bus
+                    fetchOpCode <= fetchLDD;
+                    addrBusLock <= '1';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlDualOperandOpCodeWidth) = cpuOpSTxY then
+                    -- Register puts the byte of memory to be saved onto the data bus
+                    regOpCode <= regHalfOut;
+                    if instructionBus(2) = 1 then
+                        regOpCode <= regHalfOutH;
+                    else
+                        regOpCode <= regHalfOutL;
+                    end if;
+                    regAddrBus <= instructionBus(4 downto 3);
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit does nothing other than latch the address bus
+                    fetchOpCode <= fetchSTD;
+                    addrBusLock <= '1';                   
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpADDx then
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluADD;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpSUBx then
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluSUB;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpANDx then
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluAND;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpXORx then
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluXOR;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlSingOperandOpCodeWidth) = cpuOpORx  then
+                    -- Load target register to data bus, selecting the correct 8-bit register
+                    if instructionBus(0) = 1 then
+                        regOpCode <= regHalfOutH;
+                    else
+                        regOpCode <= regHalfOutL;
+                    end if;
+                    regAddrBus <= instructionBus(2 downto 1);
+                    -- ALU stores data bus to accumulator temporary register
+                    aluOpCode <= aluOR;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlZeroOperandOpCodeWidth) = cpuOpNOTQ then
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluNOT;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlZeroOperandOpCodeWidth) = cpuOpINCQ then
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluINC;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                elsif instuctionBus(fetchInstructionWidth-1 downto fetchInstructionWidth-controlZeroOperandOpCodeWidth) = cpuOpDECQ then
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluDEC;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                else  -- This covers the NOP, JMPX, CJMPX, MVxQ, MVQx, and MVFx cases.
+                    -- Registers do nothing
+                    regOpCode <= regNOP;
+                    regAddrBus <= (others => '0');
+                    -- ALU does nothing
+                    aluOpCode <= aluNOP;
+                    -- Fetch Unit does nothing
+                    fetchOpCode <= fetchNOP;
+                    addrBusLock <= '0';
+                end if;
+        end case;
+    end process;
+
+    stateMachineProcess : process (clk, rst, nextState) is
+    begin
+        if rst then
+            stateMachine <= fetch;
+        elsif rising_edge(clk) then:
+            case stateMachine is
+                when fetch =>
+                    if nextState <= '1' then
+                        stateMachine <= load;
+                    end if;
+                when load =>
+                    if nextState <= '1' then
+                        stateMachine <= execute;
+                    end if;
+                when execute =>
+                    if nextState <= '1' then
+                        stateMachine <= fetch;
+                    end if;
+            end case;
+            nextState <= '0';
+        end if;
+    end process;
+
+
+end rtl;
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,70 @@
+# Zephyr Control Unit
+
+## Register Naming Schema
+There are four 16-bit general-purpose registers, which are accessible as 8-bit values on the data bus.
+The are named as followed:
+ - PC (Program Counter)
+ - A
+ - B
+ - C
+
+The upper or lower byte in a register is indicated by appending either H (high byte) or L (low byte).
+
+In addition to these registers, the MV instruction can reference the two 8-bit accumulator registers:
+ - Q - Accumulator (Read and Write)
+ - F - Accumulator flags (Read-Only)
+
+## Opcode Breakdown
+Most OpCodes are 5-bits long, with the remaining space occupied by register
+addresses. The two exceptions to this at present are LDxY and STxY, which 
+are only 3-bits long so as to allow space to address the necessary registers.
+
+When discussing instructions, x/X and y/Y are used as placeholders for the first and second 
+operand respectively. Lowercase is used for an 8-bit register, and uppercase is used for a 16-bit register.
+
+All instructions are 8-bits long, though the LDLx (Load Literal to x) instruction involves loading
+the next byte in memory after the instruction as a literal value.
+
+The following table describes the opcodes, and the layout of the binary encoding for each instruction.
+ - `0` and `1` characters are fixed zero or one in the instruction.
+ - `x`, `X`, and `Y` characters indicate sections where the corresponding register address is encoded.
+ - `-` characters indicate a bit that is not used in the decoding stage of an instruction. These should be left as 0 as a matter of course.
+
+| OpCode   | Operand 0        | Operand 1         | Binary Code     | Description                                                               |
+| -------- | ---------------- | ----------------- | --------------- | ------------------------------------------------------------------------- |
+| `NOP`    |                  |                   | `0000 0---`     | No operation                                                              |
+| `LDxY`   | 8-bit register   | 16-bit register   | `110x xxYY`     | Load register x with data at address Y                                    |
+| `STxY`   | 8-bit register   | 16-bit register   | `111x xxYY`     | Load register x with data at address Y                                    |
+| `LDLx`   | 8-bit register   |                   | `0000 1xxx`     | Load register x with next byte in memory                                  |
+| `JMPX`   | 16-bit register  |                   | `0001 0-XX`     | Jump to address stored in register X                                      |
+| `CJMPX`  | 16-bit register  |                   | `0001 1-XX`     | Conditional jump to address stored in register X if ALU zero flag is set  |
+| `MVxQ`   | 8-bit register   |                   | `0010 0xxx`     | Move value in register x to Q                                             |
+| `MVQx`   | 8-bit register   |                   | `0010 0xxx`     | Move value in Q to register x                                             |
+| `MVFx`   | 8-bit register   |                   | `0010 0xxx`     | Move value in F to x                                                      |
+| `ADDx`   | 8-bit register   |                   | `0011 0xxx`     | Unsigned addition of register x to Q                                      |
+| `SUBx`   | 8-bit register   |                   | `0011 1xxx`     | Unsigned subtraction of register x to Q                                   |    
+| `ANDx`   | 8-bit register   |                   | `0100 0xxx`     | Bitwise AND of register x and Q                                           |
+| `NOTQ`   |                  |                   | `0100 1---`     | Bitwise NOT of Q                                                          |
+| `XORx`   | 8-bit register   |                   | `0101 0xxx`     | Bitwise XOR of register x and Q                                           |
+| `ORx`    | 8-bit register   |                   | `0101 1xxx`     | Bitwise OR of register x and Q                                            |
+| `INCQ`   |                  |                   | `0110 0---`     | Unsigned increment Q                                                      |
+| `DECQ`   |                  |                   | `0110 1---`     | Unsigned decrement Q                                                      |
+
+Registers are addressed with the following addresses
+| Register Name | Address (Bin) | Width  |     
+| ------------- | ------------- | ------ |
+| PC            | 00            | 16-bit |
+| PCL           | 000           | 8-bit  |
+| PCH           | 001           | 8-bit  |
+| A             | 01            | 16-bit |
+| AL            | 010           | 8-bit  |
+| AH            | 011           | 8-bit  |
+| B             | 10            | 16-bit |
+| BL            | 010           | 8-bit  |
+| BH            | 011           | 8-bit  |
+| C             | 11            | 16-bit |
+| CL            | 010           | 8-bit  |
+| CH            | 011           | 8-bit  |
+
+Any additional space in the 8-bit opcode after the instruction and registers
+are encoded are filled with zeros.
